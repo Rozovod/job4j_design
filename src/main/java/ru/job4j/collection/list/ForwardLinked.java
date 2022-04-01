bf971ee7437963137294bb5914db0ee5a025f3b6
Rozovod <rozovod.store@mail.ru>
2022-04-01 12:29:54 +0300

    6. Перевернуть связанный список [#161 #282728]

--- src/main/java/ru/job4j/collection/list/ForwardLinked.java
+++ src/main/java/ru/job4j/collection/list/ForwardLinked.java
@@ -20,19 +20,21 @@ public class ForwardLinked<T> implements Iterable<T> {
     }
 
     public boolean revert() {
+        boolean rsl = true;
         if (head == null || head.next == null) {
-            return false;
+            rsl = false;
+        } else {
+            Node<T> previous = null;
+            Node<T> current = head;
+            while (current != null) {
+                Node<T> next = current.next;
+                current.next = previous;
+                previous = current;
+                current = next;
+            }
+            head = previous;
         }
-        Node<T> previous = null;
-        Node<T> current = head;
-        while (current != null) {
-            Node<T> next = current.next;
-            current.next = previous;
-            previous = current;
-            current = next;
-        }
-        head = previous;
-        return true;
+        return rsl;
     }
 
     public void addFirst(T value) {
