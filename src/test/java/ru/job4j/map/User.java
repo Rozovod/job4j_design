a2f1226749b60e11c21169f2eb102b0e57447a8d
Rozovod <rozovod.store@mail.ru>
2022-04-10 12:49:18 +0300

    2. Без переопределения equals и hashCode [#1005]

--- src/test/java/ru/job4j/map/User.java
+++ src/test/java/ru/job4j/map/User.java
@@ -1,6 +1,9 @@
 package ru.job4j.map;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class User {
     private String name;
@@ -11,5 +14,15 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    public static void main(String[] args) {
+        Calendar date = new GregorianCalendar(1990, Calendar.MAY, 7);
+        User userFirst = new User("Ivan", 1, date);
+        User userSecond = new User("Ivan", 1, date);
+        Map<User, Object> map = new HashMap<>();
+        map.put(userFirst, new Object());
+        map.put(userSecond, new Object());
+        System.out.println(map);
     }
 }
