373395531c65c433db73aec5d5ef3c7e6ec3a1ef
Rozovod <rozovod.store@mail.ru>
2022-04-18 10:31:33 +0300

    1. Создать элементарную структуру дерева [#1711 #290286]

--- src/main/java/ru/job4j/tree/SimpleTree.java
+++ src/main/java/ru/job4j/tree/SimpleTree.java
@@ -1,6 +1,7 @@
 package ru.job4j.tree;
 
 import java.util.*;
+import java.util.function.Predicate;
 
 public class SimpleTree<E> implements Tree<E> {
     private final Node<E> root;
@@ -11,29 +12,35 @@ public class SimpleTree<E> implements Tree<E> {
 
     @Override
     public boolean add(E parent, E child) {
-        boolean rsl = false;
         Optional<Node<E>> rslFindParent = findBy(parent);
-        if (findBy(child).isEmpty() && rslFindParent.isPresent()) {
-            Node<E> descendant = new Node<>(child);
-            rslFindParent.get().children.add(descendant);
-            rsl = true;
+        boolean rsl = findBy(child).isEmpty() && rslFindParent.isPresent();
+        if (rsl) {
+            rslFindParent.get().children.add(new Node<>(child));
         }
         return rsl;
     }
 
     @Override
     public Optional<Node<E>> findBy(E value) {
+        return findByPredicate(el -> el.value.equals(value));
+    }
+
+    private Optional<Node<E>> findByPredicate(Predicate<Node<E>> condition) {
         Optional<Node<E>> rsl = Optional.empty();
         Queue<Node<E>> data = new LinkedList<>();
         data.offer(this.root);
         while (!data.isEmpty()) {
             Node<E> el = data.poll();
-            if (el.value.equals(value)) {
+            if (condition.test(el)) {
                 rsl = Optional.of(el);
                 break;
             }
             data.addAll(el.children);
         }
         return rsl;
+    }
+
+    public boolean isBinary() {
+        return (findByPredicate(desc -> desc.children.size() <= 2)).isPresent();
     }
 }
