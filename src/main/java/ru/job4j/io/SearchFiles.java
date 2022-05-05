090406ce2f01abb2f340b4f6d928dcd46e014de6
Rozovod <rozovod.store@mail.ru>
2022-05-05 16:42:49 +0300

    4.1. Сканирование файловой системы. [#106929]

---
+++ src/main/java/ru/job4j/io/SearchFiles.java
@@ -0,0 +1,31 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class SearchFiles extends SimpleFileVisitor<Path> {
+    List<Path> list = new ArrayList<>();
+    Predicate<Path> predicate;
+
+    public SearchFiles(Predicate<Path> predicate) {
+        this.predicate = predicate;
+    }
+
+    public List<Path> getList() {
+        return list;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+        if (predicate.test(file)) {
+            list.add(file);
+        }
+        return FileVisitResult.CONTINUE;
+    }
+}
