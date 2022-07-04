012e745b4d3152471bc8196a632249ec149ae481
Rozovod <rozovod.store@mail.ru>
2022-07-04 17:21:59 +0300

    0.2. PreparedStatement [#379307]

---
+++ src/main/java/ru/job4j/jdbc/spammer/ImportDB.java
@@ -0,0 +1,82 @@
+package ru.job4j.jdbc.spammer;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class ImportDB {
+    private Properties cfg;
+    private String dump;
+
+    public ImportDB(Properties cfg, String dump) {
+        this.cfg = cfg;
+        this.dump = dump;
+    }
+
+    private boolean correctTxt(String line) {
+        String[] ln = line.split(";", 2);
+        if (!line.contains(";") || ln[0].isEmpty() || ln[1].isEmpty()) {
+            throw new IllegalArgumentException("Нарушение шаблона имя;email");
+        }
+        return line.contains(";");
+    }
+
+    public List<User> load() throws IOException {
+        List<User> users = new ArrayList<>();
+        try (BufferedReader rd = new BufferedReader(new FileReader(dump))) {
+            rd.lines()
+                    .filter(l -> !l.startsWith(";"))
+                    .filter(l -> !l.isEmpty())
+                    .filter(this::correctTxt)
+                    .map(l -> l.split(";", 2))
+                    .filter(i -> i.length == 2)
+                    .forEach(i -> users.add(new User(i[0], i[1])));
+        }
+        return users;
+    }
+
+    public void save(List<User> users) throws ClassNotFoundException, SQLException {
+        Class.forName(cfg.getProperty("jdbc.driver"));
+        try (Connection cnt = DriverManager.getConnection(
+                cfg.getProperty("jdbc.url"),
+                cfg.getProperty("jdbc.username"),
+                cfg.getProperty("jdbc.password")
+        )) {
+            for (User user : users) {
+                try (PreparedStatement ps = cnt.prepareStatement(
+                        "insert into users(name, email) values(?, ?)")) {
+                    ps.setString(1, user.name);
+                    ps.setString(2, user.email);
+                    ps.execute();
+                }
+            }
+        }
+    }
+
+    private static class User {
+        String name;
+        String email;
+
+        public User(String name, String email) {
+            this.name = name;
+            this.email = email;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Properties cfg = new Properties();
+        try (InputStream in = ImportDB.class.getClassLoader().getResourceAsStream("app.properties")) {
+            cfg.load(in);
+        }
+        ImportDB db = new ImportDB(cfg, "./data/dump.txt");
+        db.save(db.load());
+    }
+}
