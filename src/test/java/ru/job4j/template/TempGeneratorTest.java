8e1d8df639c4c1954a4c16ce8099420141cccce0
Rozovod <rozovod.store@mail.ru>
2022-08-19 16:36:32 +0300

    3. Шаблонизатор. [#855]

---
+++ src/test/java/ru/job4j/template/TempGeneratorTest.java
@@ -0,0 +1,44 @@
+package ru.job4j.template;
+
+import org.junit.jupiter.api.Disabled;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@Disabled
+class TempGeneratorTest {
+
+    @Disabled
+    public void whenAllValid() {
+        Generator generator = new TempGenerator();
+        Map<String, String> args = new HashMap<>();
+        args.put("name", "Petr Arsentev");
+        args.put("subject", "you");
+        String template = "I am a ${name}, Who are ${subject}?";
+        assertThat(generator.produce(template, args)).isEqualTo("I am a Petr Arsentev, Who are you?");
+    }
+
+    @Disabled
+    public void whenMoreArguments() {
+        Generator generator = new TempGenerator();
+        Map<String, String> args = new HashMap<>();
+        args.put("name", "Petr Arsentev");
+        args.put("subject", "you");
+        args.put("time", "now");
+        String template = "I am a ${name}, Who are ${subject}?";
+        assertThrows(IllegalArgumentException.class, () -> generator.produce(template, args));
+    }
+
+    @Disabled
+    public void whenMoreElTemplate() {
+        Generator generator = new TempGenerator();
+        Map<String, String> args = new HashMap<>();
+        args.put("name", "Petr Arsentev");
+        args.put("subject", "you");
+        String template = "I am a ${name}, Who are ${subject}? What are you doing ${time}?";
+        assertThrows(IllegalArgumentException.class, () -> generator.produce(template, args));
+    }
+}
