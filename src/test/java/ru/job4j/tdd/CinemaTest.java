ccb0ced0c8dc0c799338270fe6745670a1f8ee1f
Rozovod <rozovod.store@mail.ru>
2022-08-19 12:28:30 +0300

    2. Что такое TDD? [#4918]

---
+++ src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -0,0 +1,77 @@
+package ru.job4j.tdd;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.util.Calendar;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@Disabled
+class CinemaTest {
+
+    @Test
+    public void whenBuy() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2022, Calendar.AUGUST, 19, 19, 20);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        assertThat(ticket).isNull();
+    }
+
+    @Test
+    public void whenFind() {
+        Cinema cinema = new Cinema3D();
+        cinema.add(new Session3D());
+        List<Session> sessions = cinema.find(session -> true);
+        assertThat(sessions).isNull();
+    }
+
+    @Disabled
+    public void whenInvalidPlace() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2022, Calendar.AUGUST, 19, 19, 20);
+        assertThrows(IllegalArgumentException.class, () -> cinema.buy(account, -1, 1, date));
+    }
+
+    @Disabled
+    public void whenInvalidColumnInPlace() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2022, Calendar.AUGUST, 19, 19, 20);
+        assertThrows(IllegalArgumentException.class, () -> cinema.buy(account, 1, -1, date));
+    }
+
+    @Disabled
+    public void whenNotFind() {
+        Cinema cinema = new Cinema3D();
+        cinema.add(new Session3D());
+        List<Session> sessions = cinema.find(session -> false);
+        assertThat(sessions).isNull();
+    }
+
+    @Disabled
+    public void whenSamePlace() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2022, Calendar.AUGUST, 19, 19, 20);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        assertThrows(IllegalArgumentException.class, () -> cinema.buy(account, 1, 1, date));
+    }
+
+    @Disabled
+    public void whenInvalidDate() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, Calendar.AUGUST, 19, 19, 20);
+        assertThrows(IllegalArgumentException.class, () -> cinema.buy(account, 1, 1, date));
+    }
+}
