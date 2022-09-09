dd2a7b2b9497359f526c60c4422c622a70f305d2
Rozovod <rozovod.store@mail.ru>
2022-09-09 14:37:14 +0300

    2. Парковка машин [#853 #340250]

--- src/main/java/ru/job4j/design/lsp/parking/CarParking.java
+++ src/main/java/ru/job4j/design/lsp/parking/CarParking.java
@@ -18,6 +18,39 @@ public class CarParking implements IParking {
 
     @Override
     public boolean park(AbstractCar car) {
-        return false;
+        boolean rsl = false;
+        if (validateOnDuplicate(car)) {
+            if (validatePassengerCarOnPassengerParking(car)) {
+                passengerCarParking.add(car);
+                countPassengerCars--;
+                rsl = true;
+            } else if (validateTruckOnTruckParking(car)) {
+                truckParking.add(car);
+                countTrucks--;
+                rsl = true;
+            } else if (validateTruckOnPassengerParking(car)) {
+                passengerCarParking.add(car);
+                countPassengerCars = countPassengerCars - car.getSize();
+                rsl = true;
+            }
+        }
+        return rsl;
+    }
+
+    private boolean validateOnDuplicate(AbstractCar car) {
+        return !passengerCarParking.contains(car) && !truckParking.contains(car);
+    }
+
+    private boolean validatePassengerCarOnPassengerParking(AbstractCar car) {
+        return car.getSize() == PassengerCar.SIZE && countPassengerCars > 0;
+    }
+
+    private boolean validateTruckOnTruckParking(AbstractCar car) {
+        return car.getSize() > PassengerCar.SIZE && countTrucks > 0;
+    }
+
+    private boolean validateTruckOnPassengerParking(AbstractCar car) {
+        return car.getSize() > PassengerCar.SIZE && countTrucks == 0
+                && countPassengerCars >= car.getSize();
     }
 }
